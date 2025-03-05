ef7065d330c84f1ffd3eead6f6b81e2d7d62caf9
Tran Quy Thanh <tranquythanh@Trans-Mac-mini.local>
2025-03-05 09:20:59 +0700

    fix ui sidebar buss

--- lib/src/widgets/side_bar_moon/model/tree_node_model.dart
+++ lib/src/widgets/side_bar_moon/model/tree_node_model.dart
@@ -1,4 +1,5 @@
-import 'package:animated_tree_view/tree_view/tree_node.dart';
+// import 'package:animated_tree_view/tree_view/tree_node.dart';
+import 'package:easy_sidemenu/easy_sidemenu.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
@@ -6,18 +7,18 @@ import 'package:flutter/widgets.dart';
 class TreeNodeExt extends Equatable {
   TreeNodeExt({
     required this.name,
-    String? key,
-    dynamic data,
     this.pathRouter,
     this.icon,
     this.parentPathRouter,
     this.parent,
+    this.children = const [],
   });
   String? pathRouter;
   String? parentPathRouter;
   Widget? icon;
   String name;
-  TreeNode<TreeNodeExt>? parent;
+  TreeNodeExt? parent;
+  List<TreeNodeExt> children;
   @override
   List<Object?> get props => [
         pathRouter,
